9ebc2762fe9eeb0bf07edeb7c4432051b87d2c60
jmendes16 <joel.mendes@multiverse.io>
2024-02-28 18:14:15 +0000

    modified:   dashboard.py updated with MVP

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import altair as alt
 
 # set up page
 st.set_page_config(
-    page_title="1st for EPA fails",
+    page_title="ADFL4 EPA fails",
     layout="wide",
     initial_sidebar_state="expanded")
 
@@ -26,9 +26,9 @@ df_KSB_categories = pd.DataFrame({
 })
 df_KSBs = pd.merge(df_KSBs,df_KSB_categories,how='left',on='ksb')
 
-
+# the filters
 with st.sidebar:
-    st.title('1st for EPA fails')
+    st.title('ADFL4 fails')
     
     assessment_method = list(df_KSB_categories.assessment_method.unique())[::-1]
     
@@ -38,3 +38,29 @@ with st.sidebar:
 
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+
+# the graphs
+def make_barchart(input_df, input_y, input_x, input_color_theme):
+    barchart = alt.Chart(input_df).mark_bar().encode(
+            y=alt.Y(f'{input_y}', axis=alt.Axis(title="Occurence", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            x=alt.X(f'{input_x}', 
+                    axis=alt.Axis(title="KSBs", titleFontSize=18, titlePadding=15, titleFontWeight=900),
+                    sort=alt.EncodingSortField(field=f'{input_x}', op=f'{input_y}', order='ascending')),
+            stroke=alt.value('black'),
+            strokeWidth=alt.value(0.25),
+            color=input_color_theme
+        ).properties(width=900, height=800
+        ).configure_axis(
+        labelFontSize=12,
+        titleFontSize=12
+        ) 
+    return barchart
+
+# dashboard layout
+col = st.columns((1,1), gap='medium')
+
+with col[0]:
+    st.markdown('### KSB Occurences in Fail reports')
+
+    barchart = make_barchart(df_selected_KSBs_sorted, 'count', 'ksb', 'ksb')
+    st.altair_chart(barchart, use_container_width=True)
